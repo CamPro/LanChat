6de7fab7fd0effe9cbd124257028b1a6ee87d956
cam <campxpro@gmail.com>
2021-12-30 15:30:11 +0700

    Done. Project

---
+++ LanChat/LanChat.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace LanChat
+{
+    partial class LanChat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonServer = new System.Windows.Forms.Button();
+            this.buttonClient = new System.Windows.Forms.Button();
+            this.richTextServer = new System.Windows.Forms.RichTextBox();
+            this.richTextClient = new System.Windows.Forms.RichTextBox();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.buttonServerSend = new System.Windows.Forms.Button();
+            this.buttonClientSend = new System.Windows.Forms.Button();
+            this.textBoxClient = new System.Windows.Forms.TextBox();
+            this.comboListIP = new System.Windows.Forms.ComboBox();
+            this.buttonDisconect = new System.Windows.Forms.Button();
+            this.buttonListen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonServer
+            // 
+            this.buttonServer.Location = new System.Drawing.Point(13, 70);
+            this.buttonServer.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonServer.Name = "buttonServer";
+            this.buttonServer.Size = new System.Drawing.Size(184, 186);
+            this.buttonServer.TabIndex = 1;
+            this.buttonServer.Text = "Server Start";
+            this.buttonServer.UseVisualStyleBackColor = true;
+            this.buttonServer.Click += new System.EventHandler(this.buttonServer_Click);
+            // 
+            // buttonClient
+            // 
+            this.buttonClient.Location = new System.Drawing.Point(13, 305);
+            this.buttonClient.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonClient.Name = "buttonClient";
+            this.buttonClient.Size = new System.Drawing.Size(184, 215);
+            this.buttonClient.TabIndex = 2;
+            this.buttonClient.Text = "Client Start";
+            this.buttonClient.UseVisualStyleBackColor = true;
+            this.buttonClient.Click += new System.EventHandler(this.buttonClient_Click);
+            // 
+            // richTextServer
+            // 
+            this.richTextServer.Location = new System.Drawing.Point(204, 70);
+            this.richTextServer.Name = "richTextServer";
+            this.richTextServer.Size = new System.Drawing.Size(296, 186);
+            this.richTextServer.TabIndex = 3;
+            this.richTextServer.Text = "";
+            // 
+            // richTextClient
+            // 
+            this.richTextClient.Location = new System.Drawing.Point(204, 305);
+            this.richTextClient.Name = "richTextClient";
+            this.richTextClient.Size = new System.Drawing.Size(296, 186);
+            this.richTextClient.TabIndex = 4;
+            this.richTextClient.Text = "";
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(204, 262);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(215, 23);
+            this.textBoxServer.TabIndex = 5;
+            this.textBoxServer.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxServer_KeyUp);
+            // 
+            // buttonServerSend
+            // 
+            this.buttonServerSend.Location = new System.Drawing.Point(425, 262);
+            this.buttonServerSend.Name = "buttonServerSend";
+            this.buttonServerSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonServerSend.TabIndex = 6;
+            this.buttonServerSend.Text = "Send";
+            this.buttonServerSend.UseVisualStyleBackColor = true;
+            this.buttonServerSend.Click += new System.EventHandler(this.buttonServerSend_Click);
+            // 
+            // buttonClientSend
+            // 
+            this.buttonClientSend.Location = new System.Drawing.Point(425, 497);
+            this.buttonClientSend.Name = "buttonClientSend";
+            this.buttonClientSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonClientSend.TabIndex = 8;
+            this.buttonClientSend.Text = "Send";
+            this.buttonClientSend.UseVisualStyleBackColor = true;
+            this.buttonClientSend.Click += new System.EventHandler(this.buttonClientSend_Click);
+            // 
+            // textBoxClient
+            // 
+            this.textBoxClient.Location = new System.Drawing.Point(204, 497);
+            this.textBoxClient.Name = "textBoxClient";
+            this.textBoxClient.Size = new System.Drawing.Size(215, 23);
+            this.textBoxClient.TabIndex = 7;
+            this.textBoxClient.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxClient_KeyUp);
+            // 
+            // comboListIP
+            // 
+            this.comboListIP.FormattingEnabled = true;
+            this.comboListIP.Location = new System.Drawing.Point(13, 13);
+            this.comboListIP.Name = "comboListIP";
+            this.comboListIP.Size = new System.Drawing.Size(184, 24);
+            this.comboListIP.TabIndex = 9;
+            // 
+            // buttonDisconect
+            // 
+            this.buttonDisconect.Location = new System.Drawing.Point(366, 13);
+            this.buttonDisconect.Name = "buttonDisconect";
+            this.buttonDisconect.Size = new System.Drawing.Size(134, 35);
+            this.buttonDisconect.TabIndex = 10;
+            this.buttonDisconect.Text = "Disconnect";
+            this.buttonDisconect.UseVisualStyleBackColor = true;
+            this.buttonDisconect.Click += new System.EventHandler(this.buttonDisconect_Click);
+            // 
+            // buttonListen
+            // 
+            this.buttonListen.Location = new System.Drawing.Point(204, 12);
+            this.buttonListen.Name = "buttonListen";
+            this.buttonListen.Size = new System.Drawing.Size(156, 35);
+            this.buttonListen.TabIndex = 11;
+            this.buttonListen.Text = "Server Listen again";
+            this.buttonListen.UseVisualStyleBackColor = true;
+            this.buttonListen.Click += new System.EventHandler(this.buttonListen_Click);
+            // 
+            // LanChat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 554);
+            this.Controls.Add(this.buttonListen);
+            this.Controls.Add(this.buttonDisconect);
+            this.Controls.Add(this.comboListIP);
+            this.Controls.Add(this.buttonClientSend);
+            this.Controls.Add(this.textBoxClient);
+            this.Controls.Add(this.buttonServerSend);
+            this.Controls.Add(this.textBoxServer);
+            this.Controls.Add(this.richTextClient);
+            this.Controls.Add(this.richTextServer);
+            this.Controls.Add(this.buttonClient);
+            this.Controls.Add(this.buttonServer);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "LanChat";
+            this.Text = "Chat On Lan";
+            this.Load += new System.EventHandler(this.LanChat_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonServer;
+        private System.Windows.Forms.Button buttonClient;
+        private System.Windows.Forms.RichTextBox richTextServer;
+        private System.Windows.Forms.RichTextBox richTextClient;
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.Button buttonServerSend;
+        private System.Windows.Forms.Button buttonClientSend;
+        private System.Windows.Forms.TextBox textBoxClient;
+        private System.Windows.Forms.ComboBox comboListIP;
+        private System.Windows.Forms.Button buttonDisconect;
+        private System.Windows.Forms.Button buttonListen;
+    }
+}
+
---
+++ LanChat/LanChat.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LanChat
+{
+    public partial class LanChat : Form
+    {
+        public LanChat()
+        {
+            InitializeComponent();
+        }
+
+        Socket _listener = null;
+        Socket _socket = null;
+        StateObject _state = new StateObject();
+        IPEndPoint _endpoint = new IPEndPoint(IPAddress.Any, 1308);
+
+        private void LanChat_Load(object sender, EventArgs e)
+        {
+            comboListIP.Items.Clear();
+
+            foreach (IPAddress _ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+            {
+                if (_ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    comboListIP.Items.Add(_ip.ToString());
+                }
+            }
+            comboListIP.SelectedIndex = 0;
+        }
+
+        private void buttonServer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_socket != null && _socket.Connected) return;
+
+                buttonServer.BackColor = Color.Red;
+
+                _endpoint = new IPEndPoint(IPAddress.Parse(comboListIP.Text), 1308);
+                // tcp sử dụng đồng thời hai socket: 
+                // một socket để chờ nghe kết nối, một socket để gửi/nhận dữ liệu
+                // socket listener này chỉ làm nhiệm vụ chờ kết nối từ Client
+                _listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // yêu cầu hệ điều hành cho phép chiếm dụng cổng tcp 1308
+                // server sẽ nghe trên tất cả các mạng mà máy tính này kết nối tới
+                // chỉ cần gói tin tcp đến cổng 1308, tiến trình server sẽ nhận được
+                _listener.Bind(_endpoint);
+
+                // bắt đầu lắng nghe chờ các gói tin tcp đến cổng 1308
+                _listener.Listen(10);
+
+                // tcp đòi hỏi một socket thứ hai làm nhiệm vụ gửi/nhận dữ liệu
+                // socket này được tạo ra bởi lệnh Accept
+                _socket = _listener.Accept();
+
+                _socket.BeginReceive(_state.buffer, 0, StateObject.BufferSize, SocketFlags.None, new AsyncCallback(ServerArrived), _state);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void buttonListen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_listener == null) return;
+
+                // bắt đầu lắng nghe chờ các gói tin tcp đến cổng 1308
+                _listener.Listen(10);
+
+                // tcp đòi hỏi một socket thứ hai làm nhiệm vụ gửi/nhận dữ liệu
+                // socket này được tạo ra bởi lệnh Accept
+                _socket = _listener.Accept();
+
+                _socket.BeginReceive(_state.buffer, 0, StateObject.BufferSize, SocketFlags.None, new AsyncCallback(ServerArrived), _state);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void buttonServerSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_socket == null || !_socket.Connected) return;
+
+                string text = textBoxServer.Text;
+                // biến đổi chuỗi thành mảng byte
+                var sendBuffer = Encoding.ASCII.GetBytes(text);
+                // gửi mảng byte trên đến tiến trình server
+                _socket.Send(sendBuffer);
+                // không tiếp tục gửi dữ liệu nữa
+                //_socket.Shutdown(SocketShutdown.Send);
+                richTextServer.SelectionColor = Color.Black;
+                richTextServer.AppendText(text);
+                richTextServer.AppendText(Environment.NewLine);
+
+                textBoxServer.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void ServerArrived(IAsyncResult iar)
+        {
+            try
+            {
+                if (!_socket.Connected)
+                {
+                    // Disconnected
+                    buttonServer.BeginInvoke(new MethodInvoker(delegate () { buttonServer.Text = "Disconnected"; }));
+                    OnDisconect();
+                    return;
+                }
+                else if (_socket.Poll(5000, SelectMode.SelectRead) && _socket.Available == 0)
+                {
+                    // Disconnected
+                    buttonServer.BeginInvoke(new MethodInvoker(delegate () { buttonServer.Text = "Disconnected"; }));
+                    OnDisconect();
+                    return;
+                }
+
+                // Retrieve the state object and the client socket
+                // from the asynchronous state object.  
+                _state = (StateObject)iar.AsyncState;
+                string msg = string.Empty;
+
+                // Read data from the remote device.  
+                int bytesRead = _socket.EndReceive(iar);
+                if (bytesRead > 0)
+                {
+                    msg = Encoding.ASCII.GetString(_state.buffer, 0, bytesRead);
+                    // There might be more data, so store the data received so far.  
+                    _state.sb.Append(msg);
+                    //  Get the rest of the data.  
+                    _socket.BeginReceive(_state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ServerArrived), _state);
+                }
+                else
+                {
+                    // All the data has arrived; put it in response.  
+                    if (_state.sb.Length > 1)
+                    {
+
+                    }
+                    // Signal that all bytes have been received.
+                }
+
+                richTextServer.BeginInvoke(new MethodInvoker(delegate ()
+                {
+                    richTextServer.SelectionColor = Color.Blue;
+                    richTextServer.AppendText(msg);
+                    richTextServer.AppendText(Environment.NewLine);
+                }));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void buttonClient_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_socket != null && _socket.Connected) return;
+
+                buttonClient.BackColor = Color.Red;
+
+                _endpoint = new IPEndPoint(IPAddress.Parse(comboListIP.Text), 1308);
+                // khởi tạo object của lớp socket để sử dụng dịch vụ Tcp
+                // lưu ý SocketType của Tcp là Stream 
+                _socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+                // tạo kết nối tới Server
+                _socket.Connect(_endpoint);
+
+                _socket.BeginReceive(_state.buffer, 0, StateObject.BufferSize, SocketFlags.None, new AsyncCallback(ClientArrived), _state);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void buttonClientSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_socket == null || !_socket.Connected) return;
+
+                string text = textBoxClient.Text;
+                // biến đổi chuỗi thành mảng byte
+                var sendBuffer = Encoding.ASCII.GetBytes(text);
+                // gửi mảng byte trên đến tiến trình server
+                _socket.Send(sendBuffer);
+                // không tiếp tục gửi dữ liệu nữa
+                //_socket.Shutdown(SocketShutdown.Send);
+
+                richTextClient.SelectionColor = Color.Black;
+                richTextClient.AppendText(text);
+                richTextClient.AppendText(Environment.NewLine);
+
+                textBoxClient.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void ClientArrived(IAsyncResult ar)
+        {
+            try
+            {
+                if (!_socket.Connected)
+                {
+                    // Disconnected
+                    buttonClient.Text = "Disconnected";
+                    OnDisconect();
+                    return;
+                }
+                else if (_socket.Poll(5000, SelectMode.SelectRead) && _socket.Available == 0)
+                {
+                    // Disconnected
+                    buttonClient.Text = "Disconnected";
+                    OnDisconect();
+                    return;
+                }
+
+                // Retrieve the state object and the client socket
+                // from the asynchronous state object.  
+                _state = (StateObject)ar.AsyncState;
+                string msg = string.Empty;
+
+                // Read data from the remote device.  
+                int bytesRead = _socket.EndReceive(ar);
+                if (bytesRead > 0)
+                {
+                    msg = Encoding.ASCII.GetString(_state.buffer, 0, bytesRead);
+                    // There might be more data, so store the data received so far.  
+                    _state.sb.Append(msg);
+                    //  Get the rest of the data.  
+                    _socket.BeginReceive(_state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ClientArrived), _state);
+                }
+                else
+                {
+                    // All the data has arrived; put it in response.  
+                    if (_state.sb.Length > 1)
+                    {
+
+                    }
+                    // Signal that all bytes have been received.
+                }
+
+                richTextClient.BeginInvoke(new MethodInvoker(delegate ()
+                {
+                    richTextClient.SelectionColor = Color.Blue;
+                    richTextClient.AppendText(msg);
+                    richTextClient.AppendText(Environment.NewLine);
+                }));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void buttonDisconect_Click(object sender, EventArgs e)
+        {
+            OnDisconect();
+        }
+
+        private void OnDisconect()
+        {
+            if (_socket == null || !_socket.Connected) return;
+
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Disconnect(true);
+            _socket.Close();
+        }
+
+        private void textBoxServer_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonServerSend_Click(null, null);
+            }
+        }
+
+        private void textBoxClient_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonClientSend_Click(null, null);
+            }
+        }
+    }
+
+    public class StateObject
+    {
+        // Client socket.  
+        public Socket workSocket = null;
+        // Size of receive buffer.  
+        public const int BufferSize = 256;
+        // Receive buffer.  
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.  
+        public StringBuilder sb = new StringBuilder();
+    }
+}
